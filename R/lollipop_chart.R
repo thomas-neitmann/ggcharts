19facf42af1a657f0c49d928d93c6969d820b601
thomas-neitmann <th.neitmann@gmail.com>
2020-01-04 14:18:08 +0100

    Add parameter description and example to lollipop_chart() documentation

--- R/lollipop_chart.R
+++ R/lollipop_chart.R
@@ -2,6 +2,27 @@
 #'
 #' Easily create a lollipop chart using ggplot2
 #'
+#' @param data Dataset to use for the bar chart
+#' @param x The x variable
+#' @param y numeric. If y is missing then it will be assigned the number of records in each group of y
+#' @param ... Additional arguments passed to aes()
+#' @param line_size character. Size of the lollipop 'stick'
+#' @param line_color character. Color of the lollipop 'stick'
+#' @param point_size character. Size of the lollipop 'head'
+#' @param point_color character. Color of the lollipop 'head'
+#' @param sort logical. Should the data be sorted before plotting?
+#' @param horizontal logical. Should coord_flip() be added to the plot
+#' @param limit integer. If a value for limit is provided only the first limit records will be displayed
+#'
+#' @author Thomas Neitmann
+#'
+#' @examples
+#' library(dplyr)
+#' data(biomedicalrevenue)
+#' biomedicalrevenue %>%
+#'   filter(year == 2015) %>%
+#'   bar_chart(company, year, limit = 10)
+#'
 #' @importFrom ggplot2 ggplot aes geom_point geom_segment theme theme_minimal
 #'             coord_flip element_blank
 #' @importFrom magrittr %>%
