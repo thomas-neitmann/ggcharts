635e722c8caddf2ef540ac40db9f931f0f8d8698
thomas-neitmann <th.neitmann@gmail.com>
2020-01-04 11:24:37 +0100

    Create bar_chart() and lollipop_chart() functions

---
+++ R/bar_chart.R
@@ -0,0 +1,65 @@
+#' Easy Bar Charts
+#'
+#' Easily create a bar chart
+#'
+#' @author Thomas Neitmann
+#'
+#' @param data Dataset to use for the bar chart
+#' @param x The x variable
+#' @param y numeric. If y is missing then it will be assigned the number of records in each group of y
+#' @param ... Additional arguments passed to aes()
+#' @param sort logical. Should the data be sorted before plotting?
+#' @param horizontal logical. Should coord_flip() be added to the plot
+#' @param limit integer. If a value for limit is provided only the first limit records will be displayed
+#'
+#' @examples
+#'
+#' # Plots the number of records per cut
+#' data(diamonds)
+#' bar_chart(diamonds, cut)
+#'
+#' @importFrom ggplot2 ggplot aes geom_col theme theme_minimal coord_flip
+#'             element_blank element_text margin scale_fill_manual
+#' @export
+bar_chart <- function(data, x, y, ..., bar_color = "#1F77B4", sort = TRUE,
+                      horizontal = TRUE, limit = NULL) {
+  x <- rlang::enquo(x)
+  y <- rlang::enquo(y)
+  dots <- rlang::enquos(...)
+  has_fill <- "fill" %in% names(dots)
+
+  if (sort) {
+    data <- data %>%
+      dplyr::arrange(!!y) %>%
+      dplyr::mutate(!!x := reorder(!!x, !!y))
+  }
+
+  if (!is.null(limit)) {
+    data <- tail(data, limit)
+  }
+
+  .geom_col <- quote(geom_col(width = .75))
+  if (has_fill) {
+    .geom_col$position <- "dodge"
+  } else {
+    .geom_col$fill <- quote(bar_color)
+  }
+
+  p <- ggplot(data, aes(!!x, !!y, ...)) +
+    eval(.geom_col) +
+    theme_minimal() +
+    theme(
+      panel.grid.major.y = element_blank(),
+      axis.text.y = element_text(margin = margin(r = -10))
+    )
+
+  if (horizontal) {
+    p <- p + coord_flip()
+  }
+
+  if (has_fill) {
+    p <- p + scale_fill_manual(values = matplotlib_colors)
+  }
+
+  p
+}
---
+++ R/lollipop_chart.R
@@ -0,0 +1,49 @@
+#' Lollipop Chart
+#'
+#' Easily create a lollipop chart using ggplot2
+#'
+#' @importFrom ggplot2 ggplot aes geom_point geom_segment theme theme_minimal
+#'             coord_flip element_blank
+#' @export
+lollipop_chart <- function(data, x, y, ..., line_size = 0.75,
+                           line_color = "#1F77B4", point_size = 4,
+                           point_color = line_color, sort = TRUE,
+                           horizontal = TRUE, limit = NULL) {
+  x <- rlang::enquo(x)
+  y <- rlang::enquo(y)
+  dot_names <- names(rlang::enquos(...))
+
+  if (sort) {
+    data <- data %>%
+      dplyr::arrange(!!y) %>%
+      dplyr::mutate(!!x := reorder(!!x, !!y))
+  }
+
+  if (!is.null(limit)) {
+    data <- tail(data, limit)
+  }
+
+  .geom_point <- quote(geom_point())
+  .geom_segment <- quote(
+    geom_segment(aes(y = 0, xend = !!x, yend = !!y), size = line_size)
+  )
+
+  if (!"size" %in% dot_names) {
+    .geom_point$size <- quote(point_size)
+  }
+  if (!"color" %in% dot_names) {
+    .geom_segment$color <- quote(line_color)
+    .geom_point$color <- quote(point_color)
+  }
+
+  p <- ggplot(data, aes(!!x, !!y, ...)) +
+    eval(.geom_segment) +
+    eval(.geom_point) +
+    theme_minimal() +
+    theme(panel.grid.major.y = element_blank())
+
+  if (horizontal) {
+    p <- p + coord_flip()
+  }
+  p
+}
