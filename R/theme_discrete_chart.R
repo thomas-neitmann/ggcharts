6ed3bdfc9ab0427ea6e9ffee5d4c57ab75ac4536
thomas-neitmann <th.neitmann@gmail.com>
2020-01-07 19:50:35 +0100

    Remove minor grid lines

--- R/theme_discrete_chart.R
+++ R/theme_discrete_chart.R
@@ -4,12 +4,14 @@ theme_discrete_chart <- function(horizontal, ...) {
   if (horizontal) {
     t <- theme_minimal(...) +
       theme(
+        panel.grid.minor.x = element_blank(),
         panel.grid.minor.y = element_blank(),
         panel.grid.major.y = element_blank(),
       )
   } else {
     t <- theme_minimal(...) +
       theme(
+        panel.grid.minor.y = element_blank(),
         panel.grid.minor.x = element_blank(),
         panel.grid.major.x = element_blank(),
       )
