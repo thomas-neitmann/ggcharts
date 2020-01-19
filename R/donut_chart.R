1846b2ebbe9f241476b4902fe7b37c8d3ce378d0
thomas-neitmann <th.neitmann@gmail.com>
2020-01-19 13:57:37 +0100

    Add labels to donut_chart() and check whether values add up to 100%

--- R/donut_chart.R
+++ R/donut_chart.R
@@ -1,15 +1,26 @@
-donut_chart <- function(data, x, y, donut_size = 1, ...) {
-  x <- rlang::enquo(x)
-  y <- rlang::enquo(y)
+donut_chart <- function(data, category, value, label, donut_size = 1, ...) {
+  category <- rlang::enquo(category)
+  value <- rlang::enquo(value)
+
+  sum <- data %>% summarise(sum(!!value)) %>% pull()
+  if (sum == 1) {
+    label_fun <- function(x) paste0(round(x * 100, 1), "%")
+  } else if (sum == 100) {
+    label_fun <- function(x) paste0(round(x, 1), "%")
+  } else {
+    stop("Values have to add up to either 1 or 100!")
+  }
 
   data %>%
     mutate(
-      fraction = !!y / sum(!!y),
+      fraction = !!value / sum(!!value),
       ymax = cumsum(fraction),
-      ymin = c(0, ymax[-length(ymax)])
+      ymin = c(0, ymax[-length(ymax)]),
+      label = label_fun(!!value)
     ) %>%
-    ggplot(aes(fill = !!x, ymax = ymax, ymin = ymin, xmax = 4, xmin = 3)) +
+    ggplot(aes(fill = !!category, ymax = ymax, ymin = ymin, xmax = 4, xmin = 3)) +
     geom_rect() +
+    geom_text(aes(x = 3.5, y = (ymax + ymin) / 2, label = label)) +
     coord_polar(theta = "y") +
     xlim(donut_size, 4) +
     theme_void()
