b8a83a1a6dc38c7b5610b444b1280a24daa032b4
thomas-neitmann <th.neitmann@gmail.com>
2020-01-31 20:46:26 +0100

    Add documentation for biomedicalrevenue dataset

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Top Biomedical Companies Revenues.
+#'
+#' A dataset containing the annual revenues of top biomedical companies from 2011 to 2018.
+#'
+#' @format A data frame with 224 rows and 3 variables:
+#' \describe{
+#'   \item{company}{Name of the company}
+#'   \item{year}{Fiscal year}
+#'   \item{revenue}{Revenue in billion USD}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/List_of_largest_biomedical_companies_by_revenue}
+"biomedicalrevenue"
