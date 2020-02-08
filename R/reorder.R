7079359ffa49577e6d194b74b3f62a043f55160e
thomas-neitmann <th.neitmann@gmail.com>
2020-02-08 20:36:07 +0100

    Remove tidytext dependency

--- R/reorder.R
+++
@@ -1,8 +0,0 @@
-reorder <- function(x, by) {
-  factor(x, levels = x[order(by, na.last = FALSE)])
-}
-
-reorder_within <- function(x, by, within, sep = "___") {
-  new_x <- paste(x, within, sep = sep)
-  reorder(new_x, by)
-}
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,7 @@
 pt2mm <- function(x) {
   x / 2.835
 }
+
+reorder <- function(x, by) {
+  factor(x, levels = x[order(by, na.last = FALSE)])
+}
