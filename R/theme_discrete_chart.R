d84e41a714967dc2b2a3d10078a50942f0518b9d
thomas-neitmann <th.neitmann@gmail.com>
2020-02-12 20:38:41 +0100

    Turn into a proper theme and remove *all* grid lines

--- R/theme_discrete_chart.R
+++ R/theme_discrete_chart.R
@@ -1,27 +1,22 @@
 #' @importFrom ggplot2 theme_minimal theme element_blank element_rect
 #'             element_text margin
-theme_discrete_chart <- function(horizontal, ...) {
+theme_discrete_chart <- function(base_size = 14, base_family = "",
+                                 base_line_size = base_size/22,
+                                 base_rect_size = base_size/22) {
 
-  if (horizontal) {
-    t <- theme_minimal(base_size = 14, ...) +
-      theme(
-        panel.grid.minor.x = element_blank(),
-        panel.grid.minor.y = element_blank(),
-        panel.grid.major.y = element_blank()
-      )
-  } else {
-    t <- theme_minimal(...) +
-      theme(
-        panel.grid.minor.y = element_blank(),
-        panel.grid.minor.x = element_blank(),
-        panel.grid.major.x = element_blank()
-      )
-  }
-  t + theme(
-    strip.background = element_rect(fill = "gray", color = "gray"),
-    strip.text = element_text(margin = margin(1, 0, 1, 0, "mm")),
-    axis.line.x = element_line(color = "darkgray"),
-    axis.ticks.x = element_line(color = "darkgray"),
-    axis.ticks.length.x = unit(2, "mm")
-  )
+  theme_minimal(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size,
+    base_rect_size = base_rect_size
+  ) +
+    theme(
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank(),
+      strip.background = element_rect(fill = "gray", color = "gray"),
+      strip.text = element_text(margin = margin(1, 0, 1, 0, "mm")),
+      axis.line.x = element_line(color = "darkgray"),
+      axis.ticks.x = element_line(color = "darkgray"),
+      axis.ticks.length.x = unit(2, "mm")
+    )
 }
