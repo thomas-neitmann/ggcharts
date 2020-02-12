88b8072dee890fc192bbe5b1c7ddb274e596367d
thomas-neitmann <th.neitmann@gmail.com>
2020-02-12 20:34:08 +0100

    Add proper axis to theme

--- R/theme_discrete_chart.R
+++ R/theme_discrete_chart.R
@@ -19,6 +19,9 @@ theme_discrete_chart <- function(horizontal, ...) {
   }
   t + theme(
     strip.background = element_rect(fill = "gray", color = "gray"),
-    strip.text = element_text(margin = margin(1, 0, 1, 0, "mm"))
+    strip.text = element_text(margin = margin(1, 0, 1, 0, "mm")),
+    axis.line.x = element_line(color = "darkgray"),
+    axis.ticks.x = element_line(color = "darkgray"),
+    axis.ticks.length.x = unit(2, "mm")
   )
 }
