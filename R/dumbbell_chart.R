734c2c9f8755f84d6d15cdadcedc1481a0fdd432
thomas-neitmann <th.neitmann@gmail.com>
2020-03-01 21:08:22 +0100

    Add documentation for `dumbbell_chart()`

--- R/dumbbell_chart.R
+++ R/dumbbell_chart.R
@@ -1,3 +1,38 @@
+#' Dumbbell Chart
+#'
+#' Easily create a dumbbell chart
+#'
+#' @param data Dataset to use for plot
+#' @param x Variable representing the categories to plot
+#' @param y1 Variable representing the dumbbell end
+#' @param y2 Variable representing the dumbbell start
+#' @param line_size \code{numeric}. Line width
+#' @param line_color \code{character}. Line color
+#' @param point_size \code{numeric}. Point size
+#' @param point_colors \code{numeric}. Point color
+#' @param sort \code{logical}. Should the data be sorted before plotting?
+#' @param horizontal \code{logical}. Should the plot be displayed horizontally?
+#' @param limit \code{integer}. f a value for limit is provided only the first limit records will be displayed
+#' @param legend \code{logical}. Should a legend be displayed?
+#'
+#' @examples
+#' if (requireNamespace("tidyr")) {
+#'   library(magrittr)
+#'   data(biomedicalrevenue)
+#'
+#'   biomedicalrevenue %>%
+#'     dplyr::filter(year %in% c(2011, 2018)) %>%
+#'     tidyr::pivot_wider(
+#'       values_from = revenue,
+#'       names_from = year,
+#'       names_prefix = "revenue_"
+#'     ) %>%
+#'     dplyr::filter(revenue_2011 > 40, revenue_2018 > 40) %>%
+#'     dumbbell_chart(company, revenue_2011, revenue_2018)
+#' }
+#'
+#' @import ggplot2
+#' @export
 dumbbell_chart <- function(data, x, y1, y2, line_size = 1.5, line_color = "gray",
                            point_size = 4, point_colors = c("#1F77B4", "#FF7F0E"),
                            sort = TRUE, horizontal = TRUE, limit = NULL,
