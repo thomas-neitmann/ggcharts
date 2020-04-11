2fffd0fd64226fb9f4ecadc53f507087634fdd33
thomas-neitmann <th.neitmann@gmail.com>
2020-04-11 17:07:50 +0200

    Document S4 class and methods

--- R/PyramidChart.R
+++ R/PyramidChart.R
@@ -1,25 +1,52 @@
-setOldClass("patchwork")
-setOldClass("theme")
-setOldClass("ScaleContinuousPosition")
-setOldClass("labels")
+methods::setOldClass("patchwork")
+methods::setOldClass("theme")
+methods::setOldClass("ScaleContinuousPosition")
+methods::setOldClass("labels")
 
-PyramidChart <- setClass(
+#' An S4 Class Representing a Pyramid Chart
+#'
+#' @slot plot The pyramid chart created with \code{pyramic_chart}
+#' @importFrom methods new
+PyramidChart <- methods::setClass(
   "PyramidChart",
   slots = list(plot = "patchwork")
 )
 
-setMethod("+", signature("PyramidChart", "theme"), function(e1, e2) {
+#' Change PyramidChart theme
+#'
+#' @param e1 An object of class \code{PyramidChart}
+#' @param e2 An object of class \code{theme}
+#'
+#' @rdname PyramidChart-class
+methods::setMethod("+", signature("PyramidChart", "theme"), function(e1, e2) {
   PyramidChart(plot = add_theme(e1@plot, e2))
 })
 
-setMethod("+", signature("PyramidChart", "ScaleContinuousPosition"), function(e1, e2) {
+#' Change PyramidChart scale
+#'
+#' @param e1 An object of class \code{PyramidChart}
+#' @param e2 An object of class \code{ScaleContinuousPosition}
+#'
+#' @rdname PyramidChart-class
+methods::setMethod("+", signature("PyramidChart", "ScaleContinuousPosition"), function(e1, e2) {
   PyramidChart(plot = add_scale(e1@plot, e2))
 })
 
-setMethod("+", signature("PyramidChart", "labels"), function(e1, e2) {
+#' Change PyramidChart labels
+#'
+#' @param e1 An object of class \code{PyramidChart}
+#' @param e2 An object of class \code{labels}
+#'
+#' @rdname PyramidChart-class
+methods::setMethod("+", signature("PyramidChart", "labels"), function(e1, e2) {
   PyramidChart(plot = add_labels(e1@plot, e2))
 })
 
-setMethod("show", signature("PyramidChart"), function(object) {
+#' Print PyramidChart
+#'
+#' @param object An object of class \code{PyramidChart}
+#'
+#' @rdname PyramidChart-class
+methods::setMethod("show", signature("PyramidChart"), function(object) {
   print(object@plot)
 })
