b85e30a8482c682ea3f8c2faa48c9f456dbdde43
Thomas Neitmann <th.neitmann@gmail.com>
2020-04-18 08:51:34 +0200

    Readd `pyramid_theme()`
    This has been erroneously delete when merging the master branch

--- R/theme.R
+++ R/theme.R
@@ -27,3 +27,35 @@ theme_ggcharts <- function(base_size = 14,
       axis.text.y = element_text(color = "black")
     )
 }
+
+pyramid_theme <- function(side = c("left", "right")) {
+  side <- match.arg(side)
+  if (side == "left") {
+    axis_text_y <- element_blank()
+    plot_margin <- margin(5, 0, 5, 5)
+    plot_title_hjust <- 1
+  } else {
+    axis_text_y <- element_text(
+      hjust = .5,
+      color = "black",
+      margin = margin(l = 10, r = 10)
+    )
+    plot_margin <- margin(5, 5, 5, 0)
+    plot_title_hjust <- 0
+  }
+
+  theme_minimal(base_size = 13) +
+    theme(
+      axis.title.y = element_blank(),
+      axis.text.y = axis_text_y,
+      axis.title.x = element_blank(),
+      axis.ticks.y = element_blank(),
+      plot.margin = plot_margin,
+      strip.text = element_text(hjust = 0, size = 14, face = "bold"),
+      panel.grid = element_blank(),
+      plot.title = element_text(hjust = plot_title_hjust, margin = margin()),
+      axis.ticks = element_line(color = "darkgray"),
+      axis.line.x = element_line(color = "darkgray"),
+      axis.ticks.length.x = unit(1.5, "mm")
+    )
+}
