1d5243dc9b66ba765c6d343e8a165def0c2fe8bb
Thomas Neitmann <th.neitmann@gmail.com>
2020-05-16 12:17:07 +0200

    Add documentation to `line_chart()` and export the function

--- R/line_chart.R
+++ R/line_chart.R
@@ -1,3 +1,52 @@
+#' Line Chart
+#'
+#' Easily create a line chart
+#'
+#' @param data Dataset used for the line chart
+#' @param x `numeric` column of `data`
+#' @param y `numeric` column of `data` or a `tdiyselect` expression
+#' @param group `character` or `factor` column of `data` defining individual lines
+#' @param line_color `character`. Line color
+#' @param line_size `character` Line width in mm
+#'
+#' @details
+#' For plotting multiple lines `line_chart()` can handle data in long or wide format.
+#' If the data is in long format pass the variable that identifies individual lines
+#' to the `group` argument. If the data is in wide format pass a selection of variables
+#' to the `y` argument.
+#'
+#' @return An object of class `ggplot`
+#'
+#' @author Thomas Neitmann
+#'
+#' @examples
+#' library(dplyr)
+#' data("biomedicalrevenue")
+#' data("revenue_wide")
+#'
+#' line_chart(revenue_wide, year, Roche)
+#'
+#' line_chart(revenue_wide, year, Roche, line_size = 1.5)
+#'
+#' line_chart(revenue_wide, year, Roche, line_color = "darkorange")
+#'
+#' ## Plot multiple lines (data is in long format)
+#' biomedicalrevenue %>%
+#'   filter(company %in% c("Roche", "Novartis", "Bayer")) %>%
+#'   line_chart(year, revenue, group = company)
+#'
+#' ## Plot multiple lines (data in wide format, i.e. one column per line)
+#' ## Select multiple columns with `c()`
+#' line_chart(revenue_wide, year, c(Roche, Novartis, Bayer))
+#'
+#' ## Select all columns from Novartis to Sanofi suing `:`
+#' line_chart(revenue_wide, year, Novartis:Sanofi)
+#'
+#' ## Select all columns starting with "B"
+#' line_chart(revenue_wide, year, starts_with("B"))
+#'
+#' @md
+#' @export
 line_chart <- function(data, x, y, group, line_color = "auto", line_size = 1) {
   x <- rlang::enquo(x)
   y <- rlang::enquo(y)
