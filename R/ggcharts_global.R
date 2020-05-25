1ec60f6ca9b735ffbc770f86b8c5fd615f9a3f37
Thomas Neitmann <th.neitmann@gmail.com>
2020-05-25 17:09:55 +0200

    Save Okabe-Ito palette

--- R/ggcharts_global.R
+++ R/ggcharts_global.R
@@ -15,5 +15,6 @@ ggcharts_global$color_palettes <- list(
   "theme_ggcharts" = c("#1F77B4", "#ECA400", "#483D3F", "#B8B8D1", "#A41623"),
   "theme_hermit" = c("#94C1E0", "#FFC09F", "#F06449", "#82A7A6", "#805D93"),
   "theme_nightblue" = c("#D81E5B", "#848C8E", "#ECA400", "#FAF0CA", "#7E2E84"),
-  "theme_ng" = c("darkorange", "#6B9080", "#FFDDA1", "#FFFFFC", "#3E7CB1")
+  "theme_ng" = c("darkorange", "#6B9080", "#FFDDA1", "#FFFFFC", "#3E7CB1"),
+  "okabe_ito" = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#000000")
 )
