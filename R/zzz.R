86c0c9c3dbbbde824936673a5635fdec9d9115c7
Thomas Neitmann <th.neitmann@gmail.com>
2020-05-27 16:43:35 +0200

    Reactivate showtext with work around for pkgdown

--- R/zzz.R
+++ R/zzz.R
@@ -1,27 +1,36 @@
-# .onLoad <- function(libname, pkgname) {
-#   font_dir <- system.file("fonts", package = "ggcharts")
-#   sysfonts::font_paths(file.path(font_dir, "IBMPlexSans"))
-#   sysfonts::font_paths(file.path(font_dir, "OpenSans"))
-#
-#   sysfonts::font_add(
-#     family = "IBM Plex Sans",
-#     regular = "IBMPlexSans-Regular.ttf",
-#     bold = "IBMPlexSans-SemiBold.ttf",
-#     italic = "IBMPlexSans-Italic.ttf",
-#     bolditalic = "IBMPlexSans-SemiBoldItalic.ttf"
-#   )
-#
-#   sysfonts::font_add(
-#     family = "Open Sans",
-#     regular = "OpenSans-Regular.ttf",
-#     bold = "OpenSans-SemiBold.ttf",
-#     italic = "OpenSans-Italic.ttf",
-#     bolditalic = "OpenSans-SemiBoldItalic.ttf"
-#   )
-#
-#   showtext::showtext_auto(TRUE)
-# }
-#
-# .onUnload <- function(libpath) {
-#   showtext::showtext_auto(FALSE)
-# }
+.onLoad <- function(libname, pkgname) {
+  font_dir <- system.file("fonts", package = "ggcharts")
+  sysfonts::font_paths(file.path(font_dir, "IBMPlexSans"))
+  sysfonts::font_paths(file.path(font_dir, "OpenSans"))
+
+  sysfonts::font_add(
+    family = "IBM Plex Sans",
+    regular = "IBMPlexSans-Regular.ttf",
+    bold = "IBMPlexSans-SemiBold.ttf",
+    italic = "IBMPlexSans-Italic.ttf",
+    bolditalic = "IBMPlexSans-SemiBoldItalic.ttf"
+  )
+
+  sysfonts::font_add(
+    family = "Open Sans",
+    regular = "OpenSans-Regular.ttf",
+    bold = "OpenSans-SemiBold.ttf",
+    italic = "OpenSans-Italic.ttf",
+    bolditalic = "OpenSans-SemiBoldItalic.ttf"
+  )
+
+  if (Sys.getenv("IN_PKGDOWN") != "") {
+    showtext::showtext_opts(dpi = 192)
+    suppressMessages(trace(
+      what = grDevices::png,
+      exit = showtext::showtext_begin,
+      print = FALSE
+    ))
+  }
+
+  showtext::showtext_auto(TRUE)
+}
+
+.onUnload <- function(libpath) {
+  showtext::showtext_auto(FALSE)
+}
